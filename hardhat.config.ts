8d86000a2bed1945bfe2f1e62c1206b735f05620
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-11-03 21:04:10 +0000

    Deploy and verify basicNFT

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,15 @@
+import "dotenv/config"
 import "hardhat-deploy"
 import "solidity-coverage"
 import "@typechain/hardhat"
 import "hardhat-gas-reporter"
 import "@nomicfoundation/hardhat-toolbox"
 import { HardhatUserConfig } from "hardhat/config"
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY!
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL!
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
@@ -17,8 +23,13 @@ const config: HardhatUserConfig = {
         },
         goerli: {
             chainId: 5,
-            url: "",
-            accounts: [],
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+        mainnet: {
+            chainId: 1,
+            url: MAINNET_RPC_URL,
+            accounts: [PRIVATE_KEY],
         },
     },
     namedAccounts: {
@@ -33,6 +44,11 @@ const config: HardhatUserConfig = {
         outputFile: "gas-report.txt",
         noColors: true,
         // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY!,
+        },
     },
 }
 
